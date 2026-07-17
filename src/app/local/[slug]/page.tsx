6dcdd998fd35deed7c6459388ef48908002badf5
Robson Svicero <robsonsvicero@outlook.com>
2026-07-17 15:02:42 -0300

    fix overlay establishment

--- src/app/local/[slug]/page.tsx
+++ src/app/local/[slug]/page.tsx
@@ -160,7 +160,7 @@ export default async function LocalDetailPage({ params }: PageProps) {
       <section className="relative overflow-hidden">
         <div className="absolute inset-0">
           <Image src={imageSrc} alt={establishment.name} fill unoptimized={Boolean(establishment.image_cover_url)} className="object-cover object-center" priority />
-          <div className="absolute inset-0 bg-black/70" />
+          <div className="absolute inset-0 bg-black/60" />
         </div>
 
         <div className="relative mx-auto flex min-h-160 max-w-300 flex-col justify-between px-6 py-20 md:px-10 lg:px-12">
